145a83d1b62d3c95be1a43b88ad7bdd15012bfe7
Luís Gomes <luis.gomes@liferay.com>
2022-04-14 10:05:43 -0300

    Base template to start the challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-const express = require("express")
-const app = express()
-app.listen(3001)
+const express = require("express");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+function checksExistsUserAccount(req, res, next) {
+  // Complete aqui
+}
+
+app.post("/users", (req, res) => {
+  // Complete aqui
+});
+
+app.get("/todos", checksExistsUserAccount, (req, res) => {
+  // Complete aqui
+});
+
+app.post("/todos", checksExistsUserAccount, (req, res) => {
+  // Complete aqui
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (req, res) => {
+  // Complete aqui
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (req, res) => {
+  // Complete aqui
+});
+
+app.delete("/todos/:id", checksExistsUserAccount, (req, res) => {
+  // Complete aqui
+});
+
+app.listen(3001);
